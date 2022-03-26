e630e22ec09198aa6e5356b7c06d408a46901140
gnotsfw <sirawann@kkumail.com>
2022-03-27 01:42:59 +0700

    change

--- project_mrl/settings.py
+++ project_mrl/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
 
 production_host = os.getenv('PRODUCTION_HOST')
-ALLOWED_HOSTS = [production_host] if production_host is not None else []
+ALLOWED_HOSTS = ['django-nekocha.herokuapp.com'] #[production_host] if production_host is not None else []
 
 
 # Application definition
